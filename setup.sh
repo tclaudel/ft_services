d623dd3971296a8d13d50e5fb3a063b1e129a742
Kronx12 <kronx12pro@gmail.com>
2020-07-14 11:45:57 +0200

    Add phpmyadmin

--- setup.sh
+++ setup.sh
@@ -65,6 +65,11 @@ function wordpress_service {
   kubectl apply -f $WORKING_DIR/srcs/wordpress/srcs/wordpress.yaml
 }
 
+function phpmyadmin_service {
+  docker build -t ft_phpmyadmin $WORKING_DIR/srcs/phpmyadmin
+  kubectl apply -f $WORKING_DIR/srcs/phpmyadmin/srcs/phpmyadmin.yaml
+}
+
 function install_metallb {
   kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
   kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
@@ -76,6 +81,7 @@ SERVICES=(
   nginx
   ftps
   wordpress
+  phpmyadmin
 )
 
 MINIKUBE_IP=`minikube ip`
@@ -98,4 +104,4 @@ install_metallb;
 nginx_service;
 ftps_service;
 wordpress_service;
-
+phpmyadmin_service;
