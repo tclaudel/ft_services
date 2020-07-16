06218f1416cfa4c19f5c5e8368cb59366090e0e9
Kronx12 <kronx12pro@gmail.com>
2020-07-16 14:14:01 +0200

    phpmyadmin wordpress ok

--- srcs/phpmyadmin/srcs/config.inc.php
+++ srcs/phpmyadmin/srcs/config.inc.php
@@ -30,12 +30,9 @@ $i = 1;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-<<<<<<< HEAD
-=======
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['port'] = 3306; 
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
->>>>>>> f09968068113be6d159e29892b91dd9d792ca55c
 $cfg['Servers'][$i]['compress'] = false;
 // $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
