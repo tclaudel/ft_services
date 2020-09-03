3e731509c05bfe121bf82b92e096ff7e69df741f
Thomas CLAUDEL <zekidok@gmail.com>
2020-09-03 11:16:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ function install_kubectl {
 function install_minikube {
   MINIKUBE_DIR=`ls | grep "minikube-linux-amd64"`;
   if [[ ! -n MINIKUBE_DIR ]]; then
-    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64;
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube;
+    sudo mkdir -p /urs/local/bin/;
+    sudo install minikube /usr/local/bin/
     sudo install minikube-linux-amd64 /usr/local/bin/minikube;
   fi
   check_fail "installation of minikube"
